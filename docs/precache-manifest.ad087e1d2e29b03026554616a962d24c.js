7e96e0a0a5c436eb5fc8693603c93dfca35bfc0b
Liz Kalter <43416298+LizKalter@users.noreply.github.com>
2019-12-27 13:48:30 -0500

    updating to latest

---
+++ docs/precache-manifest.ad087e1d2e29b03026554616a962d24c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec6e6ebbfb6bdc70d914f5cc813a9d2b",
+    "url": "/fcc-markdown-previewer/index.html"
+  },
+  {
+    "revision": "1731260f851275860027",
+    "url": "/fcc-markdown-previewer/static/css/main.b4d35ff5.chunk.css"
+  },
+  {
+    "revision": "7abc3ef84cf6ef2b2646",
+    "url": "/fcc-markdown-previewer/static/js/2.a69afe8b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/fcc-markdown-previewer/static/js/2.a69afe8b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1731260f851275860027",
+    "url": "/fcc-markdown-previewer/static/js/main.4c8d3c6d.chunk.js"
+  },
+  {
+    "revision": "1f321acb700c43180f57",
+    "url": "/fcc-markdown-previewer/static/js/runtime-main.fee60ce2.js"
+  }
+]);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,104 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import marked from 'marked';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+marked.setOptions({
+	breaks: true
+});
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const placeholder = 
+`# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`
+
+class Editor extends React.Component {
+	render() {
+		return (
+			<textarea id="editor" onChange={this.props.updateInput} defaultValue={placeholder}></textarea>
+		);
+	}
+}
+
+class Preview extends React.Component {
+	render() {
+		return (
+			<div id="preview" dangerouslySetInnerHTML={{__html: this.props.output}}></div>
+		);
+	}
+}
+
+class MarkdownPreviewer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			output: marked(placeholder)
+		}
+		this.updateInput = this.updateInput.bind(this);
+	}
+
+	updateInput(event) {
+		const value = event.target.value;
+		const output = marked(value);
+		this.setState({
+			output: output
+		});
+	}
+
+	render() {
+		return (
+			<div className="markdown-previewer">
+				<Editor updateInput={this.updateInput} />
+				<Preview output={this.state.output} />
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<MarkdownPreviewer />, document.getElementById('root'));
+
 serviceWorker.unregister();
